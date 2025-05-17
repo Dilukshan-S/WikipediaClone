7212669aeef157d8a08824731efd08cbdc7d61a6
Dilukshan-S <Dilukshan-S@users.noreply.github.com>
2025-05-17 06:21:27 +0000

    deploy: eb57c8df49c799fd9ef9c23b038893ab5a654d7a

--- js/script.js
+++ js/script.js
@@ -3,60 +3,74 @@ const form = document.querySelector("form");
 const searchResult = document.querySelector(".results");
 const errorMsg = document.querySelector(".alert");
 const line = document.querySelector("hr");
-const apiURL = "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch="
+const loader = document.getElementById("loader");
+const apiURL =
+  "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=";
 
-form.addEventListener("submit",(e)=>{
-    e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let searchvalue = search.value.trim();
 
-    let searchvalue = search.value;
+  if (searchvalue === "") {
+    return errorMessage("Search cannot be empty. Please enter a search term.");
+  }
 
-    if(searchvalue === ""){
-        errorMessage("Search cannot be empty. Please enter a search tearm.");
-    }
-    else{
-        getResult(searchvalue)
-    }
+  getResult(searchvalue);
 });
 
-async function getResult(searchval){
+async function getResult(searchval) {
+  showLoader(true);
+  errorMsg.classList.add("hide");
+  searchResult.innerHTML = "";
+
+  try {
     const response = await fetch(apiURL + searchval);
     const results = await response.json();
-    // javascript object notation
 
-    console.log(results);
-    if(results.query.search.length==0){
-            return errorMessage("Invalid search, please enter another search term.");
-        } else {
-                displayData(results);
-            }
+    if (results.query.search.length === 0) {
+      return errorMessage("Invalid search, please enter another search term.");
+    } else {
+      displayData(results);
+    }
+  } catch (err) {
+    errorMessage("Something went wrong. Please try again.");
+  } finally {
+    showLoader(false);
+  }
 }
 
-// error Message
+// Show/hide loader
+function showLoader(show) {
+  if (show) {
+    loader.classList.remove("d-none");
+  } else {
+    loader.classList.add("d-none");
+  }
+}
+
+// Show error
 function errorMessage(msg) {
-    errorMsg.style.display = "block";
-    line.style.display = "block";
-  
-    errorMsg.textContent = msg;
-  }
+  showLoader(false);
+  errorMsg.textContent = msg;
+  errorMsg.classList.remove("hide");
+  errorMsg.style.display = "block";
+  line.classList.add("show");
+}
 
-  //display data
-  function displayData(results){
-    line.style.display="block";
+// Show results
+function displayData(results) {
+  line.classList.add("show");
+  let output = "";
 
-    let output="";
-    results.query.search.forEach((result) => {
-        let resultURL = `https://en.wikipedia.org/?curid=${result.pageid}`;
+  results.query.search.forEach((result) => {
+    const resultURL = `https://en.wikipedia.org/?curid=${result.pageid}`;
+    output += `
+      <div class="result p-1">
+        <a href="${resultURL}" target="_blank" class="h3 fw-bold">${result.title}</a><br/>
+        <a href="${resultURL}" target="_blank" class="fs-5 text-success">${resultURL}</a>
+        <p class="fs-5">${result.snippet}...</p>
+      </div>`;
+  });
 
-        output += 
-        `   <div class="result p-1">
-            <a href="${resultURL}" target="_blank" class="h3 fw-bold">
-                ${result.title}
-            </a>
-            <br/>
-            <a href="${resultURL}" target="_blank" class="fs-5 text-success">${resultURL}</a>
-            <p class="fs-5">${result.snippet}</p>
-            </div> 
-        `;
-        searchResult.innerHTML=output;
-    });
-  }
+  searchResult.innerHTML = output;
+}
